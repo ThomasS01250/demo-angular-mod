72c71df40f2a5025cc3c0e9bd1273f7b17a81fff
Demo Presenter <demo@sncf.fr>
2026-09-21 07:49:43 +0200

    chore: update reset script

--- scripts/reset-demo.ps1
+++ scripts/reset-demo.ps1
@@ -1,5 +1,5 @@
 # Réinitialise le projet à son état initial pour rejouer la démo
 git checkout master
-git reset --hard 2633951
-git clean -fd -e DEMO_GUIDE.md -e scripts/
-Write-Host "Le projet a été réinitialisé à l'état initial pour la démo." -ForegroundColor Green
+git reset --hard initial-demo-state
+git clean -fd
+Write-Host "Le projet a été réinitialisé au tag initial-demo-state pour rejouer la démo !" -ForegroundColor Green
