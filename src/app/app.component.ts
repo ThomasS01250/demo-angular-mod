aa9199f9310d45fdcef1da1e601519210ec48b17
Demo Presenter <demo@sncf.fr>
2026-09-21 09:53:11 +0200

    style: switch WCS design tokens theme to SNCF Holding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SNCF Voyageurs - Démo Migration Copilot';
+  title = 'SNCF - Démo Migration Copilot';
 }
